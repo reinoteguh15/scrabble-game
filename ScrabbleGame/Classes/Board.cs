219bfe271592beea800d4ca23d6bd7164e28543f
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2023-10-11 00:45:25 +0700

    Modify Bag.cs and Board.cs in folder Classes
    Bag.cs

--- ScrabbleGame/Classes/Board.cs
+++ ScrabbleGame/Classes/Board.cs
@@ -1,5 +1,32 @@
 namespace ScrabbleGame.Classes;
 
+using ScrabbleGame.Enums;
+
 public class Board
 {
+	private int _boardSize;
+	private string[,] _boardLetters;
+	private Dictionary<Position, Bonus> _boardBonus;
+	
+	public Board(int boardSize)
+	{
+		_boardSize = boardSize;
+		_boardLetters = new string[_boardSize, _boardSize];
+		_boardBonus = new();
+	}
+	
+	public int GetBoardSize()
+	{
+		return _boardSize;
+	}
+	
+	public string GetLetter(Position position)
+	{
+		return _boardLetters[position.GetCoordinateX(), position.GetCoordinateY()];
+	}
+	
+	public void PlaceLetter(string letter, Position position)
+	{
+		_boardLetters[position.GetCoordinateX(), position.GetCoordinateY()] = letter;
+	}
 }
