e68c03562cee6cc44583a0c3e796f493807366f8
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2023-10-10 11:22:28 +0700

    Update GameController.cs
    - Change namespace
    - Add field _playerScore and _playerRack
    - Add method AddPlayer, and AddRackToPlayer

--- ScrabbleGame/Controller/GameController.cs
+++ ScrabbleGame/Controller/GameController.cs
@@ -1,18 +1,48 @@
-using ScrabbleGame;
-using ScrabbleGame.Enum;
+using ScrabbleGame.Classes;
+using ScrabbleGame.Enums;
 using ScrabbleGame.Interface;
 
-public class GameController
+namespace ScrabbleGame.Controller;
+
+class GameController
 {
-	private Board scrabbleBoard;
+	private Dictionary<IPlayer, int>? _playerScore;
+	private Dictionary<IPlayer, Rack>? _playerRack;
 	
 	public GameController()
 	{
-		scrabbleBoard = new Board(15);
+		_playerScore = new Dictionary<IPlayer, int>();
+		_playerRack = new Dictionary<IPlayer, Rack>();
 	}
 	
-	public int GetBoardSize()
+	public void AddPlayer(IPlayer player)
 	{
-		return scrabbleBoard.GetBoardSize();
-	} 
+		_playerScore!.Add(player,0);
+	}
+	
+	public void AddRackToPlayer(IPlayer player)
+	{
+		Rack letterRack = new();
+		Random rnd = new();
+		
+		for (int i = 0; i < letterRack.GetRackSize(); i++)
+		{
+			Letter letter = (Letter) rnd.Next(1,26);
+			letterRack.AddLetterToRack(letter.ToString(), i);
+		}
+				
+		_playerRack!.Add(player,letterRack);
+	}
+	
+	public List<string> GetPlayerRack(IPlayer player)
+	{
+		List<string> listRack = new List<string>();
+		string[] playerRack = _playerRack![player].GetRack();
+		for(int i = 0; i < playerRack.Length; i++)
+		{
+			listRack.Add(playerRack[i]);
+		}
+		
+		return listRack;
+	}
 }
