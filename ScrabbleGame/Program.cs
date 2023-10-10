460f2d8fa8303a4707647a62fe248bc4ff8080b6
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2023-10-10 11:23:08 +0700

    Modify Program.cs
    Add player to game, and show the player Rack

--- ScrabbleGame/Program.cs
+++ ScrabbleGame/Program.cs
@@ -1,21 +1,36 @@
-﻿using ScrabbleGame;
+﻿using System.ComponentModel.Design.Serialization;
+using ScrabbleGame.Classes;
+using ScrabbleGame.Controller;
+using ScrabbleGame.Interface;
 
 class Program
 {
 	static void Main()
 	{
-		GameController game = new();
-		int boardSize = game.GetBoardSize();
+		Console.WriteLine("Welcome to C# Scrabble Console Game!");
+		Console.WriteLine("====================================");
+		GameController scrabbleGame = new();	
 		
-		Console.WriteLine("-------------------------------------------------------------");
-		for (int y = 0; y < boardSize; y++)
+		IPlayer player1 = new Player("Player 1", 1);
+		IPlayer player2 = new Player("Player 2", 2);
+		scrabbleGame.AddPlayer(player1);
+		scrabbleGame.AddPlayer(player2);
+		
+		scrabbleGame.AddRackToPlayer(player1);
+		scrabbleGame.AddRackToPlayer(player2);
+		
+		List<string> player1Rack = scrabbleGame.GetPlayerRack(player1);
+		Console.WriteLine($"{player1.GetPlayerName()} Rack:");
+		foreach(string letter in player1Rack)
 		{
-			for (int x = 0; x < boardSize; x++)
-			{
-				Console.Write("| a ");
-			}
-			Console.WriteLine("|");
-			Console.WriteLine("-------------------------------------------------------------");
+			Console.Write($"{letter} ");
+		}
+		
+		List<string> player2Rack = scrabbleGame.GetPlayerRack(player2);
+		Console.WriteLine($"\n\n{player2.GetPlayerName()} Rack:");
+		foreach(string letter in player2Rack)
+		{
+			Console.Write($"{letter} ");
 		}
 		
 		
