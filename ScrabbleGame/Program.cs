fc083169550ca0930a5fefb14face3f87ea5b599
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2023-10-11 00:48:37 +0700

    Update Program.cs

--- ScrabbleGame/Program.cs
+++ ScrabbleGame/Program.cs
@@ -2,6 +2,7 @@
 using ScrabbleGame.Classes;
 using ScrabbleGame.Controller;
 using ScrabbleGame.Interface;
+using Spectre.Console;
 
 class Program
 {
@@ -15,22 +16,35 @@ class Program
 		IPlayer player2 = new Player("Player 2", 2);
 		scrabbleGame.AddPlayer(player1);
 		scrabbleGame.AddPlayer(player2);
+			
+		scrabbleGame.SetUpBag();
 		
 		scrabbleGame.AddRackToPlayer(player1);
 		scrabbleGame.AddRackToPlayer(player2);
 		
 		List<string> player1Rack = scrabbleGame.GetPlayerRack(player1);
 		Console.WriteLine($"{player1.GetPlayerName()} Rack:");
-		foreach(string letter in player1Rack)
+		var table = new Table();
+		foreach(var letter in player1Rack)
 		{
-			Console.Write($"{letter} ");
+			table.AddColumn(letter);
 		}
+		AnsiConsole.Write(table);
 		
 		List<string> player2Rack = scrabbleGame.GetPlayerRack(player2);
-		Console.WriteLine($"\n\n{player2.GetPlayerName()} Rack:");
+		Console.WriteLine($"\n{player2.GetPlayerName()} Rack:");
+		var table2 = new Table();
 		foreach(string letter in player2Rack)
 		{
-			Console.Write($"{letter} ");
+			table2.AddColumn(letter);
+		}
+		AnsiConsole.Write(table2);
+		
+		var letterBag = scrabbleGame.GetRemainingTile();
+		Console.WriteLine("\nLetter Bag:");
+		foreach(var keyVP in letterBag)
+		{
+			Console.Write($"{keyVP.Key}({keyVP.Value}), ");
 		}
 		
 		
