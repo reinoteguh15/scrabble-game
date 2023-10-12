e6959025c8afa253228354e6c34d7ac3348b4ed8
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2023-10-12 10:46:17 +0700

    Update Program.cs, make static method to simplify the program
    - DisplayBoard
    - PlacingLetter

--- ScrabbleGame/Program.cs
+++ ScrabbleGame/Program.cs
@@ -10,17 +10,17 @@ class Program
 	{
 		Console.WriteLine("Welcome to C# Scrabble Console Game!");
 		Console.WriteLine("====================================");
-		GameController scrabbleGame = new(2);	
-		
+		GameController scrabbleGame = new(2);
+
 		// Input player ID & Name
 		for (int i = 0; i < scrabbleGame.PlayersCount; i++)
 		{
-			Console.Write($"Input Player {i+1} name: ");
+			Console.Write($"Input Player {i + 1} name: ");
 			string? name = Console.ReadLine();
 			IPlayer player = new Player(name, i);
 			scrabbleGame.AddPlayer(player);
 		}
-		
+
 		// First Turn
 		// Each player take one letter tile randomly, who's the closest to "A" will start first
 		scrabbleGame.SetUpBag();
@@ -28,55 +28,61 @@ class Program
 		{
 			scrabbleGame.SetPlayerRack(player);
 		}
-		
+
 		Console.WriteLine("\n~ GAME STARTED ~");
-		Console.WriteLine("GC: Each player please take a random tile from bag");
+		Console.WriteLine("(GC): Each player please take a random tile from bag");
 		IPlayer firstPlayer = scrabbleGame.GetFirstPlayer();
 		foreach (IPlayer player in scrabbleGame.GetPlayerList())
 		{
 			string playerStartingTile = scrabbleGame.GetPlayerStartingTile(player);
 			Console.WriteLine($"Player {player.GetPlayerID() + 1} get tile {playerStartingTile}");
 		}
-		Console.WriteLine($"GC: Player {firstPlayer.GetPlayerID() + 1} has the tile closest to A, starts first!");
-		
-		
-		// while()
+		Console.WriteLine($"(GC): Player {firstPlayer.GetPlayerID() + 1} has the tile closest to A, starts first!");
+
+
+		// TO DO: While game is not finished, looping through this
 		IPlayer currentPlayer = scrabbleGame.GetCurrentPlayer();
 		Console.WriteLine($"\nPlayer {currentPlayer.GetPlayerID() + 1}'s Turn");
 		ShowPlayerRack(scrabbleGame, currentPlayer);
-				
 		Console.WriteLine();
 		DisplayBoard(scrabbleGame);
 		ShowLetterBag(scrabbleGame);
-		 
-		
+
+		// TO DO: While submit is not pressed
+		PlacingLetter(scrabbleGame, currentPlayer);
+
+		// while end
 	}
-	
+
 	public static void ShowPlayerRack(GameController game, IPlayer player)
 	{
-		List<string> playerRack = game.GetPlayerRack(player);
-		Console.WriteLine($"Player {player.GetPlayerID()+1} Rack:");
+		Rack playerRack = game.GetPlayerRack(player);
+		Console.WriteLine($"Player {player.GetPlayerID() + 1} Rack:");
 		var table = new Table();
-		foreach(var letter in playerRack)
+		foreach (var letter in playerRack.GetRack())
 		{
 			table.AddColumn(letter);
 		}
 		AnsiConsole.Write(table);
-		Console.WriteLine();
 	}
 	public static void ShowLetterBag(GameController gameController)
 	{
 		var letterBag = gameController.GetRemainingTile();
-		Console.WriteLine("\nLetter Bag:");
-		foreach(var keyVP in letterBag)
+		int total = 0;
+		Console.WriteLine($"\nLetter Bag: ");
+		foreach (var keyVP in letterBag)
 		{
 			Console.Write($"{keyVP.Key} ({keyVP.Value}), ");
+			total += keyVP.Value;
 		}
+		Console.WriteLine();
+		Console.WriteLine($"Total: {total} tiles");
 	}
 	public static void DisplayBoard(GameController gameController)
 	{
 		Board board = gameController.GetBoard();
 		int boardSize = board.GetBoardSize();
+		Console.WriteLine("  1   2   3   4   5   6   7   8   9   10  11  12  13  14  15 ");
 		Console.WriteLine("+-----------------------------------------------------------+");
 		for (int y = 0; y < boardSize; y++)
 		{
@@ -86,8 +92,83 @@ class Program
 				string letter = board.GetLetter(position) ?? " ";
 				Console.Write($"| {letter} ");
 			}
-			Console.WriteLine("|");
+			Console.WriteLine($"| {y+1}");
 			Console.WriteLine("+-----------------------------------------------------------+");
+		}
+	}
+	
+	public static void PlacingLetter(GameController gameController, IPlayer player)
+	{
+		bool isContinue = true;
+		while (isContinue)
+		{
+			Console.WriteLine();
+			Console.Write($"(GC): Player {player.GetPlayerID() + 1}, do you want to place the letter to board? (y/n): ");
+			string? wantToPlace = Console.ReadLine();
+			if (wantToPlace == "y")
+			{
+				PlaceLetter:
+				Console.Write($"(GC): Player {player.GetPlayerID() + 1}, please enter the letter you want to place: ");
+				string? letterToPlace = Console.ReadLine();
+				Console.Write($"(GC): Enter the x coordinate: ");
+				bool parseX = int.TryParse(Console.ReadLine(), out int xCoord);
+				Console.Write($"(GC): Enter the y coordinate: ");
+				bool parseY = int.TryParse(Console.ReadLine(), out int yCoord);
+
+				bool letterPlaced = gameController.PlaceLetterToBoard(player, xCoord - 1, yCoord - 1, letterToPlace!);
+				if (letterPlaced == true)
+				{
+					Console.WriteLine($"Letter {letterToPlace} has been placed at ({xCoord},{yCoord}).");
+				}
+				else
+				{
+					Console.WriteLine($"Letter {letterToPlace} doesn't exist in your Rack, please choose another letter");
+				}
+
+				DisplayBoard(gameController); Console.WriteLine();
+				ShowPlayerRack(gameController, player);
+				ShowLetterBag(gameController);
+
+				Console.Write("\nDo you want to place the letter again? (y/n): ");
+				string? playerContinue = Console.ReadLine();
+				if (playerContinue == "y" || playerContinue == "Y")
+				{
+					goto PlaceLetter;
+				}
+				else if (playerContinue == "n" || playerContinue == "N")
+				{
+					isContinue = false;
+					Console.Write("Do you want to submit the words? (y/n): ");
+					string? isSubmit = Console.ReadLine();
+					if (isSubmit == "y" || isSubmit == "Y")
+					{
+						throw new NotImplementedException();
+					}
+					else if (isSubmit == "n" || isSubmit == "N")
+					{
+						throw new NotImplementedException();
+					}
+				}
+				else
+				{
+					Console.WriteLine("No option, please choose y/n");
+					return;
+				}
+			}
+			else
+			{
+				Console.Write("Do you want to skip turn? (y/n)");
+				string? skipTurn = Console.ReadLine();
+				if (skipTurn == "y" || skipTurn == "Y")
+				{
+					isContinue = false;
+				}
+				else if (skipTurn == "n" || skipTurn == "N")
+				{
+					return;
+				}
+			}
+
 		}
 	}
 }
