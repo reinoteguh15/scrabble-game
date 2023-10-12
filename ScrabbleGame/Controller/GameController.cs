84306615246e376477d48186561acdfef2e8ca87
reinoteguh15 <reinoteguhsts15@gmail.com>
2023-10-13 00:02:51 +0700

    Refactoring the GameController.cs

--- ScrabbleGame/Controller/GameController.cs
+++ ScrabbleGame/Controller/GameController.cs
@@ -1,185 +1,95 @@
+namespace ScrabbleGame.Controller;
+
 using ScrabbleGame.Classes;
 using ScrabbleGame.Enums;
 using ScrabbleGame.Interface;
-using System;
-using System.Xml.Serialization;
-
-
-namespace ScrabbleGame.Controller;
 
 class GameController
 {
-	public int PlayersCount {get; private set;}
-	private Bag? _wordsBag;
+	public int MaxPlayer {get; private set;}
+	private List<string> _lettersBag;
 	private Board _board;
 	private IPlayer? _currentPlayer;
 	private List<IPlayer> _playerList;
-	private Dictionary<IPlayer, string> _playerStartingTile;
-	private Dictionary<IPlayer, int> _playerScore;
-	private Dictionary<IPlayer, Rack> _playerRack;
-	private Dictionary<IPlayer, PlayerTurn> _playerTurn;
-	
+	private Dictionary<IPlayer, PlayerData> _playerInfo;
 	private Dictionary<string, int> _tileQuantity;
 	private Dictionary<string, int> _tileScore;
-	private Dictionary<Position, Bonus> _letterMultiplier;
+	private Dictionary<Position, Bonus> _multiplier;
 	
-	public GameController(int numPlayers)
+	public GameController(int maxPlayers)
 	{
-		PlayersCount = numPlayers;
-		_wordsBag = new();
+		// Assign maximum number of players
+		MaxPlayer = maxPlayers;
+		
+		// Create a new list string for letters bag
+		_lettersBag = new();
+		
+		// Create a new board with size of 15 x 15
 		_board = new(15);
+		
+		// Add player to game
 		_playerList = new();
-		_playerStartingTile = new();
-		_playerScore = new();
-		_playerRack = new();
-		_playerTurn = new();
+		
+		// Assign new player info
+		_playerInfo = new();
+		
+		for (int i = 1; i <= MaxPlayer; i++)
+		{
+			Player player = new();
+			PlayerData playerData = new();
+			player.SetPlayerID(i);
+			_playerList.Add(player);
+			_playerInfo.Add(player, playerData);
+		}
+		
 		_tileQuantity = new();
 		_tileScore = new();
-		_letterMultiplier = new();
+		_multiplier = new();
 	}
 	
-	public Board GetBoard()
+	public int GetBoardSize()
 	{
-		return _board;
+		return _board.GetSize();
 	}
-	public Bag GetBag()
+	public string GetBoardLetter(Position position)
 	{
-		return _wordsBag!;
+		return _board.GetLetter(position);
 	}
 	public void SetUpBoard()
 	{
-		_board.SetUpBoard();
+		_board.SetUp();
 	}
-	public void SetUpBag()
+	public void InitializeBag()
 	{
-		foreach(var letter in Enum.GetNames(typeof(Letter)))
+		foreach (var letter in Enum.GetNames(typeof(Letter)))
 		{
-			if (letter == "E")
+			LetterQuantity letterQuantity = (LetterQuantity) Enum.Parse(typeof(LetterQuantity), letter);
+			LetterScore letterScore = (LetterScore) Enum.Parse(typeof(LetterScore), letter);
+			
+			for (int i = 0; i < (int)letterQuantity; i++)
 			{
-				_tileQuantity.Add(letter, 12);
+				_lettersBag.Add(letter);
 			}
-			else if ((letter == "A") || (letter == "I"))
-			{
-				_tileQuantity.Add(letter, 9);
-			}
-			else if (letter == "O")
-			{
-				_tileQuantity.Add(letter, 8);
-			}
-			else if ((letter == "N") || (letter == "R") || (letter == "T"))
-			{
-				_tileQuantity.Add(letter, 6);
-			}
-			else if ((letter == "D") || (letter == "L") || (letter == "S") || (letter == "U"))
-			{
-				_tileQuantity.Add(letter, 4);
-			}
-			else if (letter == "G")
-			{
-				_tileQuantity.Add(letter, 3);
-			}
-			else if(
-				(letter == "B") || (letter == "C") || (letter == "F") || (letter == "H") || (letter == "M") ||
-				(letter == "P") || (letter == "V") || (letter == "W") || (letter == "Y") || (letter == "Blank")
-			)
-			{
-				_tileQuantity.Add(letter, 2);
-			}
-			else if((letter == "J") || (letter == "K") || (letter == "Q") || (letter == "X") || (letter == "Z"))
-			{
-				_tileQuantity.Add(letter, 1);
-			}
-			else
-			{
-				return;
-			}
-		}
-		
-		foreach(var letter in Enum.GetNames(typeof(Letter)))
-		{
-			if ((letter == "Q") || (letter == "Z"))
-			{
-				_tileScore.Add(letter, 10);
-			}
-			else if ((letter == "J") || (letter == "X"))
-			{
-				_tileScore.Add(letter, 8);
-			}
-			else if (letter == "K")
-			{
-				_tileScore.Add(letter, 5);
-			}
-			else if ((letter == "F") || (letter == "H") || (letter == "V") || (letter == "W") || (letter == "Y"))
-			{
-				_tileScore.Add(letter, 4);
-			}
-			else if ((letter == "B") || (letter == "C") || (letter == "M") || (letter == "P"))
-			{
-				_tileScore.Add(letter, 3);
-			}
-			else if ((letter == "G") || (letter == "D"))
-			{
-				_tileScore.Add(letter, 2);
-			}
-			else if(
-				(letter == "A") || (letter == "E") || (letter == "I") || (letter == "L") || (letter == "N") ||
-				(letter == "O") || (letter == "R") || (letter == "S") || (letter == "T") || (letter == "U")
-			)
-			{
-				_tileScore.Add(letter, 1);
-			}
-			else if(letter == "Blank")
-			{
-				_tileScore.Add(letter, 0);
-			}
-			else
-			{
-				return;
-			}
-		}
-		
-		foreach (var keyValuePair in _tileQuantity)
-		{
-			for(int count = 0; count < keyValuePair.Value; count++)
-			{
-				_wordsBag!.AddLetterToBag(keyValuePair.Key);
-			}
+			
+			_tileQuantity.Add(letter, (int)letterQuantity);
+			_tileScore.Add(letter, (int)letterScore);
 		}
 	}
-	public bool AddPlayer(IPlayer player)
+	public void InitializeRack(IPlayer player)
 	{
-		if(! _playerScore.ContainsKey(player))
-		{
-			_playerScore.Add(player,0);
-			_playerList.Add(player);
-			return true;	
-		}
-		else
-		{
-			return false;
-		}
-	}
-	public void SetPlayerRack(IPlayer player)
-	{
-		Rack letterRack = new();
 		Random rnd = new();
 		
+		// Rack only contains 7 letters, so i < 7
 		for (int i = 0; i < 7; i++)
 		{
-			List<string> letters = _wordsBag!.GetLettersFromBag();
-			string letter = letters[rnd.Next(1,letters.Count)];
-			
-			_wordsBag.RemoveLetter(letter);
+			int randomIndex = rnd.Next(1,_lettersBag.Count);
+			string letter = _lettersBag[randomIndex];
+						
+			_lettersBag.RemoveAt(randomIndex);
+			_playerInfo[player].AddLetter(letter);
 			_tileQuantity[letter] -= 1;
-			
-			letterRack.AddLetterToRack(letter);		
 		}
 				
-		_playerRack!.Add(player,letterRack);
-	}
-	public Rack GetPlayerRack(IPlayer player)
-	{
-		return _playerRack[player];
 	}
 	public Dictionary<string, int> GetRemainingTile()
 	{
@@ -189,48 +99,47 @@ class GameController
 	{
 		return _playerList;
 	}
-	public string GetPlayerStartingTile(IPlayer player)
+	public string? GetInitialTile(IPlayer player)
 	{
-		return _playerStartingTile[player];
+		return _playerInfo[player].GetStartingTile();
 	}
-	public IPlayer GetFirstPlayer()
+	public IPlayer GetInitialPlayer()
 	{
 		int i = 0;
-		int[] startingTileScore = new int[PlayersCount];
+		int[] initialTileScore = new int[MaxPlayer];
 		
 		foreach(IPlayer player in _playerList)
-		{
-			int playerTileScore = new Random().Next(26);
-			startingTileScore[i] = playerTileScore;
+		{		
+			string? playerTile = GetInitialTile(player);
 			
-			string playerTile = Enum.GetName(typeof(Letter), playerTileScore)!;
-			_playerStartingTile.Add(player, playerTile);
+			Letter letterScore = (Letter) Enum.Parse(typeof(LetterScore), playerTile);
+			initialTileScore[i] = (int)letterScore;
 			i++;
 		}
-		int first = Array.IndexOf(startingTileScore, startingTileScore.Min());
-		_currentPlayer = _playerList[first];
-		return _playerList[first];
+		int initialPlayer = Array.IndexOf(initialTileScore, initialTileScore.Min());
+		
+		_currentPlayer = _playerList[initialPlayer];
+		return _playerList[initialPlayer];
 	}
 	public IPlayer GetCurrentPlayer()
 	{
-		return _currentPlayer!;
+		return _currentPlayer;
 	}
-	public int GetPlayerScore(IPlayer player)
-	{  
-		// TODO:
-		throw new NotImplementedException();
+	public List<string> GetPlayerRack(IPlayer player)
+	{
+		return _playerInfo[player].GetRack();
 	}
-	public bool PlaceLetterToBoard(IPlayer player, int x, int y, string letter)
+	public bool PlaceLetter(IPlayer player, int x, int y, string letter)
 	{
 		Position position = new Position(x, y);
-		Rack playerRack = GetPlayerRack(player);
+		var playerRack = GetPlayerRack(player);
 		
-		if (playerRack.ContainsLetter(letter))
+		if (playerRack.Contains(letter))
 		{
 			if ((_board.GetLetter(position) == " ") || (_board.GetLetter(position) == null))
 			{
 				_board.PlaceLetter(letter, position);
-				playerRack.RemoveLetterFromRack(letter);
+				playerRack.Remove(letter);
 				return true;
 			}
 			else
@@ -243,12 +152,9 @@ class GameController
 			return false;
 		}		
 	}
-	public PlayerTurn GetAction()
-	{
-		throw new NotImplementedException();
-	}
-	public bool isSubmit()
-	{
+	public int GetPlayerScore(IPlayer player)
+	{  
+		// TODO:
 		throw new NotImplementedException();
 	}
 	public bool isGameFinish()
