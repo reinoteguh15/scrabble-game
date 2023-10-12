626bb508fef8d9d567a213ade5e9a5e84c8f8fbf
Reinato Teguh Santoso <reinoteguhsts15@gmail.com>
2023-10-12 10:42:16 +0700

    Update Rack.cs, changing the rack from string[] into List<string>
    Add method:
    - ContainsLetter
    - RemoveLetterFromRack
    
    Modify method:
    - AddLetterToRack

--- ScrabbleGame/Classes/Rack.cs
+++ ScrabbleGame/Classes/Rack.cs
@@ -2,25 +2,37 @@ namespace ScrabbleGame.Classes;
 
 public class Rack
 {
-	private string[] _letterRack;
+	private List<string> _letterRack;
 	
 	public Rack()
 	{
-		_letterRack = new string[7];
+		_letterRack = new();
 	}
-	
-	public void AddLetterToRack(string letter, int index)
+	public bool ContainsLetter(string letter)
 	{
-		_letterRack[index] = letter;
-		
+		return _letterRack.Contains(letter);
 	}
-	
+	public void AddLetterToRack(string letter)
+	{
+		if (_letterRack.Count < 7)
+		{
+			_letterRack.Add(letter);
+		}
+		else
+		{
+			return;
+		}
+	}
+	public void RemoveLetterFromRack(string letter)
+	{
+		_letterRack.Remove(letter);
+	}
 	public int GetRackSize()
 	{
-		return _letterRack.Length;
+		return _letterRack.Count;
 	}
 	
-	public string[] GetRack()
+	public List<string> GetRack()
 	{
 		return _letterRack;
 	}
