abbb2efaa25c669b06ea6d84d7aec9aecd59f398
reinoteguh15 <reinoteguhsts15@gmail.com>
2023-10-17 22:44:21 +0700

    Modify code to get next player in Program.cs

--- ScrabbleGame/Program.cs
+++ ScrabbleGame/Program.cs
@@ -10,6 +10,7 @@ using System.Text;
 
 public partial class Program
 {
+	static IPlayer currentPlayer;
 	static List<Position> positions = new();
 	static StringBuilder sb = new();
 	static void Main()
@@ -52,93 +53,93 @@ public partial class Program
 
 		// On-going Game
 		while (!scrabbleGame.IsGameFinish())
-		{
-			PlayerTurn:
-			IPlayer currentPlayer = scrabbleGame.GetCurrentPlayer();
-			Console.WriteLine($"\nPlayer {currentPlayer.GetPlayerID()}'s Turn");
-			ShowRack(scrabbleGame, currentPlayer);
+		{	
+			while(!scrabbleGame.IsPlayerTurnFinish(scrabbleGame.GetCurrentPlayer()))
+			{
+				currentPlayer = scrabbleGame.GetCurrentPlayer();
+				Console.WriteLine($"\nPlayer {currentPlayer.GetPlayerID()}'s Turn");
+				ShowRack(scrabbleGame, currentPlayer);
 
-			Console.WriteLine($"Player {currentPlayer.GetPlayerID()}, please select your action: ");
-			Console.WriteLine("1) Place Letter");
-			Console.WriteLine("2) Skip Turn");
-			Console.WriteLine("3) Swap Letter");
-			Console.WriteLine("4) Resign");
-			Console.Write("Action: ");
-			bool actionStatus = int.TryParse(Console.ReadLine(), out int playerAction);
+				Console.WriteLine($"Player {currentPlayer.GetPlayerID()}, please select your action: ");
+				Console.WriteLine("1) Place Letter");
+				Console.WriteLine("2) Skip Turn");
+				Console.WriteLine("3) Swap Letter");
+				Console.WriteLine("4) Resign");
+				Console.Write("Action: ");
+				bool actionStatus = int.TryParse(Console.ReadLine(), out int playerAction);
+				
+				if (playerAction == 1)
+				{
+					PlacingLetter(scrabbleGame, currentPlayer);
+				}
+				else if (playerAction == 2)
+				{
+					Console.WriteLine($"Player {currentPlayer.GetPlayerID()} has skipped the turn");
+					currentPlayer = scrabbleGame.GetNextPlayer();
+					scrabbleGame.SetPlayerStatus(currentPlayer, PlayerStatus.SkipTurn);
+				}
+				else if (playerAction == 3)
+				{
+					Console.WriteLine($"Player {currentPlayer.GetPlayerID()} requests to swap letter");
+					
+					// TODO: if player resign, the next player wins
+					throw new NotImplementedException();
+				}
+				else if (playerAction == 4)
+				{
+					Console.WriteLine($"Player {currentPlayer.GetPlayerID()} has resigned");
+					scrabbleGame.SetPlayerStatus(currentPlayer, PlayerStatus.Resign);
+					
+					// TODO: if player resign, the next player wins
+					throw new NotImplementedException();
+				}
+				else
+				{
+					Console.WriteLine("\nNo option, please choose the right option!");
+				}
+				
+				scrabbleGame.SetPlayerStatus(currentPlayer, PlayerStatus.EndTurn);		
+			}
 			
-			if (playerAction == 1)
-			{
-				PlacingLetter(scrabbleGame, currentPlayer);
-			}
-			else if (playerAction == 2)
-			{
-				Console.WriteLine($"Player {currentPlayer.GetPlayerID()} has skipped the turn");
-				currentPlayer = scrabbleGame.GetNextPlayer();
-				goto PlayerTurn;
-			}
-			else if (playerAction == 3)
-			{
-				Console.WriteLine($"Player {currentPlayer.GetPlayerID()} requests to swap letter");
-				
-				// TODO: if player resign, the next player wins
-				throw new NotImplementedException();
-			}
-			else if (playerAction == 4)
-			{
-				Console.WriteLine($"Player {currentPlayer.GetPlayerID()} has resigned");
-				
-				// TODO: if player resign, the next player wins
-				throw new NotImplementedException();
-			}
-			else
-			{
-				Console.WriteLine("\nNo option, please choose the right option!");
-			}
-
 		}
 
 	}
 	
 	static void PlacingLetter(GameController gameController, IPlayer player)
 	{
-		while (!gameController.IsGameFinish())
+		bool isFinishedPlacing = false;
+		while (!isFinishedPlacing)
 		{
-			bool isTurnFinished = false;
-
-			while (!isTurnFinished)
-			{
 			PlaceLetter:
-				PutLetter(gameController, player);
-				
+			PutLetter(gameController, player);
+			
 			PlaceLetterAgain:
-				Console.Write("\nDo you want to place the letter again? (y/n): ");
-				string? playerContinue = Console.ReadLine();
+			Console.Write("\nDo you want to place the letter again? (y/n): ");
+			string? playerContinue = Console.ReadLine();
 
-				if (playerContinue == "y" || playerContinue == "Y")
-				{
-					goto PlaceLetter;
-				}
-				else if (playerContinue == "n" || playerContinue == "N")
-				{
-					isTurnFinished = true;
-					Console.Write("Do you want to submit the words? (y/n): ");
-					string? isSubmit = Console.ReadLine();
-					if (isSubmit == "y" || isSubmit == "Y")
-					{
-						SubmitLetter(gameController, player);
-					}
-					else if (isSubmit == "n" || isSubmit == "N")
-					{
-						goto PlaceLetterAgain;
-					}
-				}
-				else
-				{
-					Console.WriteLine("No option, please choose y/n");
-				}
-
+			if (playerContinue == "y" || playerContinue == "Y")
+			{
+				goto PlaceLetter;
 			}
-
+			else if (playerContinue == "n" || playerContinue == "N")
+			{
+				isFinishedPlacing = true;
+				Console.Write("Do you want to submit the words? (y/n): ");
+				string? isSubmit = Console.ReadLine();
+				if (isSubmit == "y" || isSubmit == "Y")
+				{
+					SubmitLetter(gameController, player);
+				}
+				else if (isSubmit == "n" || isSubmit == "N")
+				{
+					goto PlaceLetterAgain;
+				}
+			}
+			else
+			{
+				Console.WriteLine("No option, please choose y/n");
+			}
+			currentPlayer = gameController.GetNextPlayer();
 		}
 	}
 	static void PutLetter(GameController gameController, IPlayer player)
@@ -160,7 +161,7 @@ public partial class Program
 			if (letterPlaced == true)
 			{
 				Console.WriteLine($"Letter {letterToPlace} has been placed at ({xCoord},{yCoord}).");
-				Position position = new Position(xCoord - 1, yCoord - 1);
+				Position position = new Position(xCoord, yCoord);
 				positions.Add(position);
 				sb.Append(letterToPlace);
 
@@ -191,8 +192,8 @@ public partial class Program
 			Console.WriteLine("Move invalid! Please check the placement of your letter tile");
 		}
 
-		int playerPoint = gameController.EvaluateWords(positions, sb.ToString());
-
-		throw new NotImplementedException();
+		int playerPoint = gameController.EvaluateWords(player, positions, sb.ToString());
+		Console.WriteLine($"\nPlayer {player.GetPlayerID()}'s score: {playerPoint}");
+		gameController.SetPlayerStatus(player, PlayerStatus.EndTurn);
 	}
 }
