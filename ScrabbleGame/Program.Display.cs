38aea1e56b4fee95b3c9bc20773632198461f531
reinoteguh15 <reinoteguhsts15@gmail.com>
2023-10-17 17:35:33 +0700

    Update Program.cs and Program.Display.cs
    Move the method from Program.Display.cs to Program.cs

--- ScrabbleGame/Program.Display.cs
+++ ScrabbleGame/Program.Display.cs
@@ -4,14 +4,10 @@ using ScrabbleGame.Classes;
 using ScrabbleGame.Controller;
 using ScrabbleGame.Enums;
 using ScrabbleGame.Interface;
-using System.Text;
 using Spectre.Console;
 
 public partial class Program
 {
-	static List<Position> positions = new();
-	static StringBuilder sb = new();
-
 	static void ShowRack(GameController gameController, IPlayer player)
 	{
 		List<char> playerRack = gameController.GetPlayerRack(player);
@@ -59,97 +55,6 @@ public partial class Program
 			}
 			Console.WriteLine("|");
 			Console.WriteLine("\t+-----------------------------------------------------------+");
-		}
-	}
-
-	static void PutLetter(GameController gameController, IPlayer player)
-	{
-		Console.Write($"(GC): Player {player.GetPlayerID()}, please enter the letter you want to place: ");
-		char letterToPlace = Console.ReadKey().KeyChar;
-		Console.WriteLine();
-
-		if (gameController.GetPlayerRack(player).Contains(letterToPlace))
-		{
-
-			Console.Write($"(GC): Enter the x coordinate: ");
-			bool parseX = int.TryParse(Console.ReadLine(), out int xCoord);
-
-			Console.Write($"(GC): Enter the y coordinate: ");
-			bool parseY = int.TryParse(Console.ReadLine(), out int yCoord);
-
-			bool letterPlaced = gameController.PlaceLetter(player, (xCoord - 1), (yCoord - 1), letterToPlace);
-			if (letterPlaced == true)
-			{
-				Console.WriteLine($"Letter {letterToPlace} has been placed at ({xCoord},{yCoord}).");
-				Position position = new Position(xCoord - 1, yCoord - 1);
-				positions.Add(position);
-				sb.Append(letterToPlace);
-
-				DisplayBoard(gameController);
-			}
-			else
-			{
-				Console.WriteLine($"Tile ({xCoord},{yCoord}) has any letter in it.");
-			}
-		}
-		else
-		{
-			Console.WriteLine($"Letter {letterToPlace} doesn't exist in your Rack, please choose another letter");
-		}
-	}
-
-	static void SubmitLetter(GameController gameController, IPlayer player)
-	{
-		// TODO: evaluate the words
-		Console.WriteLine($"Player {player.GetPlayerID()} has submitted the word");
-		Console.WriteLine("Evaluating the word...");
-
-		Console.WriteLine(gameController.IsValidMove(positions));
-
-		int playerPoint = gameController.EvaluateWords(positions, sb.ToString());
-
-		throw new NotImplementedException();
-	}
-	static void PlacingLetter(GameController gameController, IPlayer player)
-	{
-		while (!gameController.IsGameFinish())
-		{
-			bool isTurnFinished = false;
-
-			while (!isTurnFinished)
-			{
-			PlaceLetter:
-				PutLetter(gameController, player);
-				
-			PlaceLetterAgain:
-				Console.Write("\nDo you want to place the letter again? (y/n): ");
-				string? playerContinue = Console.ReadLine();
-
-				if (playerContinue == "y" || playerContinue == "Y")
-				{
-					goto PlaceLetter;
-				}
-				else if (playerContinue == "n" || playerContinue == "N")
-				{
-					isTurnFinished = true;
-					Console.Write("Do you want to submit the words? (y/n): ");
-					string? isSubmit = Console.ReadLine();
-					if (isSubmit == "y" || isSubmit == "Y")
-					{
-						SubmitLetter(gameController, player);
-					}
-					else if (isSubmit == "n" || isSubmit == "N")
-					{
-						goto PlaceLetterAgain;
-					}
-				}
-				else
-				{
-					Console.WriteLine("No option, please choose y/n");
-				}
-
-			}
-
 		}
 	}
 }
--- ScrabbleGame/Program.cs
+++ ScrabbleGame/Program.cs
@@ -5,12 +5,17 @@ using ScrabbleGame.Classes;
 using ScrabbleGame.Controller;
 using ScrabbleGame.Enums;
 using ScrabbleGame.Interface;
+using System.Text;
 
 
 public partial class Program
 {
+	static List<Position> positions = new();
+	static StringBuilder sb = new();
 	static void Main()
 	{
+		
+		
 		Console.WriteLine("Welcome to C# Scrabble Console Game!");
 		Console.WriteLine("====================================");
 		
@@ -92,5 +97,102 @@ public partial class Program
 
 		}
 
+	}
+	
+	static void PlacingLetter(GameController gameController, IPlayer player)
+	{
+		while (!gameController.IsGameFinish())
+		{
+			bool isTurnFinished = false;
+
+			while (!isTurnFinished)
+			{
+			PlaceLetter:
+				PutLetter(gameController, player);
+				
+			PlaceLetterAgain:
+				Console.Write("\nDo you want to place the letter again? (y/n): ");
+				string? playerContinue = Console.ReadLine();
+
+				if (playerContinue == "y" || playerContinue == "Y")
+				{
+					goto PlaceLetter;
+				}
+				else if (playerContinue == "n" || playerContinue == "N")
+				{
+					isTurnFinished = true;
+					Console.Write("Do you want to submit the words? (y/n): ");
+					string? isSubmit = Console.ReadLine();
+					if (isSubmit == "y" || isSubmit == "Y")
+					{
+						SubmitLetter(gameController, player);
+					}
+					else if (isSubmit == "n" || isSubmit == "N")
+					{
+						goto PlaceLetterAgain;
+					}
+				}
+				else
+				{
+					Console.WriteLine("No option, please choose y/n");
+				}
+
+			}
+
+		}
+	}
+	static void PutLetter(GameController gameController, IPlayer player)
+	{
+		Console.Write($"(GC): Player {player.GetPlayerID()}, please enter the letter you want to place: ");
+		char letterToPlace = Console.ReadKey().KeyChar;
+		Console.WriteLine();
+
+		if (gameController.GetPlayerRack(player).Contains(letterToPlace))
+		{
+
+			Console.Write($"(GC): Enter the x coordinate: ");
+			bool parseX = int.TryParse(Console.ReadLine(), out int xCoord);
+
+			Console.Write($"(GC): Enter the y coordinate: ");
+			bool parseY = int.TryParse(Console.ReadLine(), out int yCoord);
+
+			bool letterPlaced = gameController.PlaceLetter(player, (xCoord - 1), (yCoord - 1), letterToPlace);
+			if (letterPlaced == true)
+			{
+				Console.WriteLine($"Letter {letterToPlace} has been placed at ({xCoord},{yCoord}).");
+				Position position = new Position(xCoord - 1, yCoord - 1);
+				positions.Add(position);
+				sb.Append(letterToPlace);
+
+				DisplayBoard(gameController);
+			}
+			else
+			{
+				Console.WriteLine($"Tile ({xCoord},{yCoord}) has any letter in it.");
+			}
+		}
+		else
+		{
+			Console.WriteLine($"Letter {letterToPlace} doesn't exist in your Rack, please choose another letter");
+		}
+	}
+	static void SubmitLetter(GameController gameController, IPlayer player)
+	{
+		// TODO: evaluate the words
+		Console.WriteLine($"\nPlayer {player.GetPlayerID()} has submitted the word");
+		Console.WriteLine("Evaluating the word...");
+
+		if(gameController.IsValidMove(positions))
+		{
+			Console.WriteLine("Move is valid !");
+		}
+		else
+		{
+			Console.WriteLine("Move invalid! Please check the placement of your letter tile");
+		}
+
+		int playerPoint = gameController.EvaluateWords(positions, sb.ToString());
+
+		throw new NotImplementedException();
 	}
 }
